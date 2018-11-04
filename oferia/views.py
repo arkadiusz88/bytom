8ff3eb91582bcdce72ba7f449969539c26fda9e3
arkadiusz88 <arkadiusz.zieba2@outlook.com>
2018-11-04 17:53:39 +0100

    Displayed values from db

--- oferia/views.py
+++ oferia/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Offer
 
 # Create your views here.
 
 def oferia_list(request):
-    return render(request, "oferia/oferia_list.html", {})
+    displayAll = Offer.objects.all()
+    return render(request, "oferia/oferia_list.html", {'allOffers': displayAll})
 
