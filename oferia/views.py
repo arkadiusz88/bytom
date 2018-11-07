8b6465b5210986826e628e90d7fb4e8b25ce43f8
arkadiusz88 <arkadiusz.zieba2@outlook.com>
2018-11-07 23:19:22 +0100

    Fixing issue with CSS file and adding method to save data form

--- oferia/views.py
+++ oferia/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Offer
 from .forms import OfferForm
-
+from django.utils import timezone
+from django.shortcuts import redirect
 # Create your views here.
 
 def oferia_list(request):
@@ -14,5 +15,14 @@ def offer_detail(request, pk):
 
 
 def offer_new_edit(request):
-    form = OfferForm
-    return render(request, "oferia/offer_edit.html", {'form' : form})
+    if request.method == 'POST':
+        form = OfferForm(request.POST)
+        if form.is_valid():
+            offer = form.save(commit = False)
+            offer.author =  request.user
+            offer.published_date = timezone.now()
+            offer.save()
+            return redirect('offer_detail', pk = offer.pk)
+    else:
+        form = OfferForm
+        return render(request, "oferia/offer_edit.html", {'form' : form})
